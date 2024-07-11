8d70406de98b1ba735de1bfbed960617e43a98a8
Sabbir Hossain <175347762+Sabbir-Hossain22@users.noreply.github.com>
2024-07-12 02:50:07 +0600

    Add files via upload

---
+++ BitwiseOperator.java
@@ -0,0 +1,19 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        
+        int a =32;
+        int b =12;
+        int c ;
+        
+        c = a & b ;
+        System.out.println("Bitwise AND : "+c);
+        
+        c = a | b ;
+        System.out.println("Bitwise OR : "+c);
+        
+        c = a ^ b ;
+        System.out.println("Bitwise XOR : "+c);
+        
+    }
+    
+}
