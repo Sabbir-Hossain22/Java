244ae0189506d9c58952c9db8de3a458fe096402
Sabbir Hossain <175347762+Sabbir-Hossain22@users.noreply.github.com>
2024-07-12 13:39:03 +0600

    Add files via upload

---
+++ BreakStatement.java
@@ -0,0 +1,20 @@
+public class BreakStatement {
+
+    public static void main(String[] args) {
+        
+        
+        for(int i = 1; i<=100; i=i+2)
+        {
+            if(i == 10)
+                
+            {
+                break; 
+            }
+            
+            System.out.println(i);
+        
+        }
+        
+        
+    }
+}
