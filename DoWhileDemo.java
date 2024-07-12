682a85462c6a2abe5069ea7a47089a9a158da1d1
Sabbir Hossain <175347762+Sabbir-Hossain22@users.noreply.github.com>
2024-07-12 15:37:16 +0600

    Add files via upload

---
+++ DoWhileDemo.java
@@ -0,0 +1,10 @@
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 1; 
+        
+        do {
+            System.out.println(i); 
+            i = i + 2; 
+        } while (i <= 100); 
+    }
+}
