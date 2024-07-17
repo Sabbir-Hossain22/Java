77286111e47e86b66779d2e640823dda801060bd
Sabbir Hossain <175347762+Sabbir-Hossain22@users.noreply.github.com>
2024-07-17 23:18:25 +0600

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,15 @@
+package BeginnerJava;
+
+public class Array3 
+{
+  public static void main (String [] args)
+  {
+	  String [] names = {"Sabbir","Hossain","Shihab","Anisul"};
+	  
+	  
+	  for(String x : names)
+	  {
+		  System.out.println(x);
+	  }
+  }
+}
