198f0ee4675e21e9692c8e3e557c6426adf4a469
Sabbir Hossain <175347762+Sabbir-Hossain22@users.noreply.github.com>
2024-07-30 19:45:05 +0600

    Add files via upload

---
+++ Lab/Lab1/question1.java
@@ -0,0 +1,16 @@
+package lab2;
+
+public class question1 {
+	
+	public static void main (String[] args)
+	{
+		double v = 22.4, t = 0.7583;
+		double avg =0;
+		
+		 avg = v/t;
+		
+		System.out.println("The average speed is> "+avg);
+		
+	}
+
+}
---
+++ Lab/Lab1/question3.java
@@ -0,0 +1,14 @@
+package lab2;
+
+public class question3 {
+	
+	public static void main (String[] args)
+	{
+		double dist = 40 * 1.6;
+		double time = 1.6763889;
+		double avg = dist / time;
+		
+		System.out.println("The average speed is : "+avg);
+	}
+
+}
