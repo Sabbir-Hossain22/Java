deaadd59b0f09991d147ae7385d55f2c91a34571
Sabbir Hossain <175347762+Sabbir-Hossain22@users.noreply.github.com>
2024-09-30 14:43:01 +0600

    Add files via upload

---
+++ Assignment-02/s1Impl.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.assignment2;
+
+
+public class s1Impl extends s1 {
+    
+}
